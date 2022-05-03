ccc2cf36086fb4d2eac82c7a3aec927eb3ba171a
김성용 <sykim@stayonbox.com>
2022-05-03 18:13:09 +0900

    add complete

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,8 +1,132 @@
 import React from 'react'
+import styled from 'styled-components';
+import { ProgressBar, Button } from 'react-bootstrap';
+import { QuestionData } from '../assets/data/questiondata'
+import { createSearchParams, useNavigate } from 'react-router-dom';
 
 const Question = () => {
 
-  return(<div>질문면입니다.</div>)
+  const [questionNo, setQuestionNo] = React.useState(0);
+  const [totalScore, setTotalScore] = React.useState([
+    {id:"EI", score:0},
+    {id:"SN", score:0},
+    {id:"TF", score:0},
+    {id:"JP", score:0},
+  ]);
+
+  // console.log(totalScore);
+
+  const navigate = useNavigate();
+
+  const handleClickAnswer = (add, type) => {
+    const newScore = totalScore.map((s) =>
+      s.id === type ? { id: s.id, score: s.score + add } : s
+    );
+    setTotalScore(newScore);
+    if (QuestionData.length !== questionNo + 1) {
+      setQuestionNo(questionNo + 1);
+    } else {
+      const mbti = newScore.reduce(
+        (acc, curr) =>
+          acc +
+          (curr.score >= 2 ? curr.id.substring(0, 1) : curr.id.substring(1, 2)),
+        ""
+      );
+      navigate({
+        pathname: "/result",
+        search: `?${createSearchParams({
+          mbti: mbti,
+        })}`,
+      });
+    }
+  };
+  // const handleClickButton = (type, no) =>{
+    
+  //   if(type === "EI"){
+  //     // 기존 스코어에 더할 값을 계산
+  //     const addScore = totalScore[0].score + no;
+  //     const newObject = { id:"EI", score: addScore};
+      
+  //     // splice로 새로운 객체를 해당 객체 대체
+  //     totalScore.splice(0, 1, newObject);
+      
+  //   }else if(type === "SN"){
+  //     const addScore = totalScore[1].score + no;
+  //     const newObject = { id:"SN", score: addScore};
+  //     totalScore.splice(1, 1, newObject);
+      
+  //   }else if(type === "TF"){
+  //     const addScore = totalScore[2].score + no;
+  //     const newObject = { id:"TF", score: addScore};
+  //     totalScore.splice(2, 1, newObject);
+      
+  //   }else if(type === "JP"){
+  //     const addScore = totalScore[3].score + no;
+  //     const newObject = { id:"JP", score: addScore};
+  //     totalScore.splice(3, 1, newObject);
+  //   }
+
+
+  //   setQuestionNo(questionNo + 1);
+  // }
+
+  // const handleClickButtonB = (type, no) =>{
+  //   if(type === "EI"){
+  //     // 기존 스코어에 더할 값을 계산
+  //     const addScore = totalScore[0].score + no;
+  //     const newObject = { id:"EI", score: addScore};
+      
+  //     // splice로 새로운 객체를 해당 객체 대체
+  //     totalScore.splice(0, 1, newObject);
+      
+  //   }else if(type === "SN"){
+  //     const addScore = totalScore[1].score + no;
+  //     const newObject = { id:"SN", score: addScore};
+  //     totalScore.splice(1, 1, newObject);
+      
+  //   }else if(type === "TF"){
+  //     const addScore = totalScore[2].score + no;
+  //     const newObject = { id:"TF", score: addScore};
+  //     totalScore.splice(2, 1, newObject);
+      
+  //   }else if(type === "JP"){
+  //     const addScore = totalScore[3].score + no;
+  //     const newObject = { id:"JP", score: addScore};
+  //     totalScore.splice(3, 1, newObject);
+  //   }    
+  //   setQuestionNo(questionNo + 1);
+  // }
+
+  return(
+    <Wrapper>
+      <ProgressBar striped variant="danger" now={(questionNo / QuestionData.length) * 100} style={{ marginTop: '20px'}} />
+      <Title> {QuestionData[questionNo].title} </Title>
+      <ButtonGroup>
+        <Button style={{ width: '40%', minHeight:"200px", fontSize:"15pt"}} onClick={()=>handleClickAnswer(1,QuestionData[questionNo].type)} >{QuestionData[questionNo].answera}</Button>
+        <Button style={{ width: '40%', minHeight:"200px", fontSize:"15pt", marginLeft:"20px"}} onClick={()=>handleClickAnswer(0,QuestionData[questionNo].type)}>{QuestionData[questionNo].answerb}</Button>
+      </ButtonGroup>
+    </Wrapper>
+  )
 }
 
 export default Question ;
+
+const Wrapper = styled.div`
+  // background-color : pink;
+  height : 100vh;
+  width : 100%;
+`
+
+const Title = styled.div`
+  font-size :  30pt;
+  text-align: center;
+  margin-top : 40px;
+  font-family: "Happiness-Sans-Regular";
+  `
+const ButtonGroup = styled.div`
+  display:flex;
+  flex-direction : row;
+  align-items : center;
+  justify-content: center;
+  font-family: "Happiness-Sans-Regular";
+`  
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,8 +1,88 @@
 import React from 'react'
 
+import styled from 'styled-components';
+import PangImage from '../assets/goimg.jpeg';
+import Button from 'react-bootstrap/Button';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+import { ResultData } from '../assets/data/resultdata'
+
 const Result = () => {
 
-  return(<div>결과화면입니다.</div>)
+  const navigate = useNavigate();
+
+  const [searchParams] = useSearchParams();
+  const mbti = searchParams.get('mbti');
+
+  const [resultData, setResultData] = React.useState({});
+
+  React.useEffect(()=>{
+    const result = ResultData.find((s) => s.best === mbti);
+    setResultData(result);
+  }, [mbti])
+
+
+console.log(mbti);
+
+  const handle = () =>{
+    // 
+    navigate('/question');
+  }
+
+  return(
+    <Wrapper>
+      <Header>예비집사 판별결과</Header>
+      <Contents>
+        <Title>결과보기</Title>
+        <LogoImage>
+          <img src={resultData.image} className="rounded-circle"  width={350} height={350}  ></img>
+        </LogoImage>
+        <Desc>
+          예비집사님과 맞는 고양이는 {resultData.name}입니다. 
+        </Desc>
+        <Button style={{fontFamily: "Happiness-Sans-Regular"}} onClick={()=>navigate('/question')} >테스트 다시하기 </Button>
+      </Contents>
+    </Wrapper>
+  )
 }
+
+
+const Wrapper = styled.div`
+  // background-color : pink;
+  height : 100vh;
+  width : 100%;
+`
+
+const Header = styled.div`
+  font-size :  40pt;
+  display : flex;
+  justify-content : center;
+  align-items: center;
+  font-family: "Happiness-Sans-Regular";
+`
+const Contents = styled.div`
+  display : flex;
+  flex-direction : column;
+  justify-content : center;
+  align-items: center;
+`
+
+const Title = styled.div`
+  font-size :  30pt;
+  margin-top : 40px;
+  font-family: "Happiness-Sans-Regular";
+  `
+  
+const LogoImage = styled.div`
+  margin-top : 10px;
+  `
+  
+  const Desc = styled.div`
+  font-size :  20pt;
+  margin-top : 10px;
+  font-family: "Happiness-Sans-Regular";
+
+`
+
 
 export default Result ;
